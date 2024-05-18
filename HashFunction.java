e3196c18350066a5185d17b65043d13613470022
buntman <148082762+buntman@users.noreply.github.com>
2024-05-18 09:08:22 +0800

    Fixed digit and midsquare function

--- HashFunction.java
+++ HashFunction.java
@@ -1,4 +1,5 @@
 public class HashFunction {
+    String [] table = new String[50];
 
      public String wordToAscii(String word) {
         StringBuilder asciiValues = new StringBuilder();
@@ -24,29 +25,53 @@ public class HashFunction {
          return key;
     }
 
-    public char digitSelection(String asciiValues) {
-        return asciiValues.charAt(0);
+    public int digitSelection(String asciiValues) { //First digit is the key
+         char x = asciiValues.charAt(0);
+         return Integer.parseInt(String.valueOf(x));
     }
 
-    public char midSquare(String asciiValues) {
+    public int midSquare(String asciiValues) {
          int left = 0;
          int right = asciiValues.length() - 1;
          int middle = (left + right) / 2;
-         return asciiValues.charAt(middle);
+         char x = asciiValues.charAt(middle);
+         int key = Integer.parseInt(String.valueOf(x));
+         key *= key;
+         if(key >= 50) {  //if the squared value is outside of range, get the left side value
+             String k = Integer.toString(key);
+             char val = k.charAt(0);
+             key = Integer.parseInt(String.valueOf(val));
+         }
+         return key;
+    }
+
+    public void addElements(String word) {
+         String asciiVal = wordToAscii(word);
+
+         int index = foldingAdding(asciiVal);
+         table[index] = word;
+    }
+
+    public void printTable() {
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(i + " " + table[i]);
+        }
     }
 
     public static void main(String[] args) {
         HashFunction user = new HashFunction();
-        String input = "ranmie";
+        String input = "ara";
         String value = user.wordToAscii(input);
-        System.out.println("ASCII value: " + value);
-        int fold = user.foldingAdding(value);
-        System.out.println("Folding and Adding: " + fold);
-        int modulo = user.moduloFunction(value);
-        System.out.println("Modulo: " + modulo);
-        char digit = user.digitSelection(value);
-        System.out.println("Digit: " + digit);
-        char mid = user.midSquare(value);
+        System.out.println("ASCII: " + value);
+//        user.addElements(input);
+//        user.printTable();
+
+
+
+        int digit = user.digitSelection(value);
+        System.out.println("Digit key: " + digit);
+
+        int mid = user.midSquare(value);
         System.out.println("Midsquare: " + mid);
     }
 }
